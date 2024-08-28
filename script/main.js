9d37bbdf412cc77c0bfd7c4a4bc274253fd06a84
IlyaShcelkanov <123357324+IlyaShcelkanov@users.noreply.github.com>
2024-08-28 22:41:20 +0300

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
     Swal.fire({
-        title: 'Вы Бережная Антонина Ефимовна?',
+        title: 'Хотите музыкальное сопровождение?',
         icon: 'warning',
         showCancelButton: true,
         confirmButtonColor: '#3085d6',
